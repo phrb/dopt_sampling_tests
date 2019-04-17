38a27eaf9aa945e7cd8d86f07316a61fe373c256
Pedro Bruel <pedro.bruel@gmail.com>
2019-04-17 21:01:48 +0200

    Add results for gaussian sampling

--- src/gaussian_sampling/allocate_g5k_resources.sh
+++ src/gaussian_sampling/allocate_g5k_resources.sh
@@ -17,4 +17,4 @@ echo "Allocating machines"
 
 #oarsub -q default -p "cluster='grisou'" -l nodes=9,walltime=14 -t deploy -r "2018-09-21 19:00:00"
 #oarsub -I -q default -p "cluster='paravance'" -l nodes=10,walltime=12 -t deploy
-oarsub -I -q default -p "cluster='graphene'" -l nodes=20,walltime=12 -t deploy
+oarsub -I -q production -p "cluster='grvingt'" -l nodes=30,walltime=3 -t deploy
